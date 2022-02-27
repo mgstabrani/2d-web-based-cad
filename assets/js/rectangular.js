44f31eb1b34c4ec65f5304ca6bc34839e7560773
ayahyaaa <k.naufalyahya@gmail.com>
2022-02-27 22:20:16 +0700

    change canvas size
    square and rectangle initialization fixed but still no square scaling

--- assets/js/rectangular.js
+++ assets/js/rectangular.js
@@ -12,8 +12,8 @@ function setRectangle() {
     isRectangle = true
 }
 
-function drawSquare(x, y, s=0.25) {
-    vertices = getVertices(x, y, s, 'square')
+function drawSquare(x, y, translation=0.25) {
+    vertices = getVertices(x, y, translation, 'square')
     
     renderAll()
     renderObject(vertices, 4, gl.TRIANGLE_FAN)
@@ -48,29 +48,30 @@ function drawRectangle(x, y) {
     points = []
 }
 
-function getVertices(x, y, s, type) {
+function getVertices(x, y, translation, type) {
     if (type === 'square') {
-        var x1 = x - s
-        var y1 = y + s
-        var x2 = x1 + s*1.1
-        var y2 = y1 - s*1.6
-        
-        return [
-            x1, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255,
-            x2, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255,
-            x2, y2, rgb[0]/255, rgb[1]/255, rgb[2]/255,
-            x1, y2, rgb[0]/255, rgb[1]/255, rgb[2]/255
-        ]
-    }
-    else if (type === 'rectangle') {
-        var x1 = x - s
-        var y1 = y + s
+        var x1 = x - translation
+        var y1 = y + translation
         
         return [
             x, y, rgb[0]/255, rgb[1]/255, rgb[2]/255,
-            x1, y, rgb[0]/255, rgb[1]/255, rgb[2]/255,
+            x, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255,
             x1, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255,
-            x, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255
+            x1, y, rgb[0]/255, rgb[1]/255, rgb[2]/255
+        ]
+    }
+    else if (type === 'rectangle') {
+        var x1 = x - translation*0.3
+        var y1 = y + translation*0.3
+        var x2 = x + translation*0.7
+        var y2 = y - translation*0.25
+        
+        return [
+            x1, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255,
+            x1, y2, rgb[0]/255, rgb[1]/255, rgb[2]/255,
+            x2, y2, rgb[0]/255, rgb[1]/255, rgb[2]/255,
+            x2, y1, rgb[0]/255, rgb[1]/255, rgb[2]/255
+            
         ]
     }
 }
